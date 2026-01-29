ff8e07e99c18c6aa502bef0eaaa9db1a6a7f0b38
Cyb3rBob <45741104+Cyb3rBob@users.noreply.github.com>
2026-01-29 15:57:36 -0600

    Update netpps.sh
    Minor updates to comments

--- netpps.sh
+++ netpps.sh
@@ -13,7 +13,7 @@
 # --- HOW TO RUN: ---
 # 1. Find your interface name by running the command:  ip link
 #    If you have more then one interface, make sure you pick the one with the correct IP Address.
-#    Run this script xample: ./netpps.sh <interface_name>  (e.g., ./netpps.sh eth0)
+#    Run this script xample: netpps.sh <interface_name>  (e.g., netpps.sh eth0)
 # 2. To stop Press: CTRL + C
 
 # Configuration
