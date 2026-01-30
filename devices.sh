693f632063b41263b019aa1114a4fe3b016f3083
Cyb3rBob <45741104+Cyb3rBob@users.noreply.github.com>
2026-01-30 10:36:55 -0600

    Update devices.sh
    Added information about using sudo command for this script.

--- devices.sh
+++ devices.sh
@@ -13,14 +13,16 @@
 # 2. Give permission so anyone can run it:  sudo chmod +x /usr/local/bin/devices.sh
 # 
 # --- HOW TO RUN: ---
-# 1. Type:  devices.sh
+# 1. Type: sudo devices.sh
+#    This script must be run with higher privliges to give the tcpdump binary the ability to capture packets
+#    and write the exporters.txt file to a restricted directory. 
 # 2. Script will automatically stop after the set number of packets is received.  By default this is 
 #    10,000 packets.  This may need to be increased or decreased depending on how much telemetry
 #    is being sent to your sensor.
 
 # Configuration
 OUTPUT_FILE="exporters.txt"
-INTERFACE="any"       
+INTERFACE="any"
 PORTS="9995 or 9997"  # change port numbers to ports used on your sensor.  (e.g., 2055, 9996 etc..)
 CAPTURE_COUNT=10000   # Increase or decrease capture count as needed based on how much traffic is 
 #                       being sent to your sensor.  (Higher = longer capture period)
